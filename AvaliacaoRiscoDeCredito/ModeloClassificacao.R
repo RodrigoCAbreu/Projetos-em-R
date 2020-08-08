ed3f29d99d15dc311bb57d4ead03b8a1ec431858
Rodrigo Abreu <rodrigo.c.abreu@hotmail.com>
2020-08-07 23:24:15 -0300

    iniciado modelo classificacao - risco de credito

---
+++ AvaliacaoRiscoDeCredito/ModeloClassificacao.R
@@ -0,0 +1,26 @@
+# Variável que controla a execução do script
+Azure <- FALSE
+
+if(Azure){
+  source("src/ClassTools.R")
+  Credit <- maml.mapInputPort(1)
+}else{
+  source("ClassTools.R")
+  Credit <- read.csv("GermanCredit.csv", header = F, stringsAsFactors = F )
+  metaFrame <- data.frame(colNames, isOrdered, I(factOrder))
+  Credit <- fact.set(Credit, metaFrame)
+  
+  # Balancear o número de casos positivos e negativos
+  Credit <- equ.Frame(Credit, 2)
+}
+
+# Transformando variáveis numéricas em variáveis categóricas
+toFactors <- c("Duration", "CreditAmount", "Age")
+maxVals <- c(100, 1000000, 100)
+facNames <- unlist(lapply(toFactors, function(x) paste(x, "_f", sep = "")))
+Credit[, facNames] <- Map(function(x, y) quantize.num(Credit[, x], maxval = y), toFactors, maxVals)
+
+# str(Credit)
+
+# Output 
+if(Azure) maml.mapOutputPort('Credit')
---
+++ AvaliacaoRiscoDeCredito/readme.txt
@@ -0,0 +1,7 @@
+### Modelo machine learning - Classificação
+
+#Fonte dos dados: https://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data)
+
+# Problema de Negócio: Criar  um  modelo  preditivo  para classificar o risco de crédito de clientes de uma instituição bancária
+
+#Este experimento visa demonstrar o processo de construção de um modelo de classificação para prever o  risco  de  concessão  de  crédito  a  clientes  de  um banco. O  dataset  contém 1000observações  e 20variáveis,  representando  osdados  de  clientes,  tais  como:  status  da  conta  corrente,  histórico  de  crédito, quantidade de crédito atual, empregabilidade, residência, idade, etc..
