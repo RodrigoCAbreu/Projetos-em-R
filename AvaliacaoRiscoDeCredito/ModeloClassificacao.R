cff516ccfcd7a143baf17150cc4a0d1f266ebbf8
Rodrigo Abreu <rodrigo.c.abreu@hotmail.com>
2020-08-09 07:57:32 -0300

    continuação modelo de classificaão

--- AvaliacaoRiscoDeCredito/ModeloClassificacao.R
+++ AvaliacaoRiscoDeCredito/ModeloClassificacao.R
@@ -24,3 +24,72 @@ Credit[, facNames] <- Map(function(x, y) quantize.num(Credit[, x], maxval = y), 
 
 # Output 
 if(Azure) maml.mapOutputPort('Credit')
+
+# Análise Exploratória de Dados
+
+# Variável que controla a execução do script
+Azure <- FALSE
+
+if(Azure){
+  source("src/ClassTools.R")
+  Credit <- maml.mapInputPort(1)
+}
+
+# Plots usando ggplot2
+library(ggplot2)
+lapply(colNames2, function(x){
+  if(is.factor(Credit[,x])) {
+    ggplot(Credit, aes_string(x)) +
+      geom_bar() + 
+      facet_grid(. ~ CreditStatus) + 
+      ggtitle(paste("Total de Credito Bom/Ruim por",x))}})
+
+# Plots CreditStatus vs CheckingAcctStat
+lapply(colNames2, function(x){
+  if(is.factor(Credit[,x]) & x != "CheckingAcctStat") {
+    ggplot(Credit, aes(CheckingAcctStat)) +
+      geom_bar() + 
+      facet_grid(paste(x, " ~ CreditStatus"))+ 
+      ggtitle(paste("Total de Credito Bom/Ruim CheckingAcctStat e",x))
+  }})
+
+# Feature Selection (Seleção de Variáveis)
+
+
+# Variavel que controla a execucao do script
+Azure <- FALSE
+
+if(Azure){
+  source("src/ClassTools.R")
+  Credit <- maml.mapInputPort(1)
+}  
+
+# Modelo randomForest para criar um plot de importância das variáveis
+library(randomForest)
+modelo <- randomForest( CreditStatus ~ .
+                        - Duration
+                        - Age
+                        - CreditAmount
+                        - ForeignWorker
+                        - NumberDependents
+                        - Telephone
+                        - ExistingCreditsAtBank
+                        - PresentResidenceTime
+                        - Job
+                        - Housing
+                        - SexAndStatus
+                        - InstallmentRatePecnt
+                        - OtherDetorsGuarantors
+                        - Age_f
+                        - OtherInstalments, 
+                        data = Credit, 
+                        ntree = 100, nodesize = 10, importance = T)
+
+varImpPlot(modelo)
+
+outFrame <- serList(list(credit.model = modelo))
+
+
+## Output 
+if(Azure) maml.mapOutputPort("outFrame")
+
